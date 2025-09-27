1b98bbe5ff29e794d8edb448c655e917582c9ea0
Sean Cooper <seancooper@usf.edu>
2025-09-26 21:04:03 -0400

    step 3

--- cc_5.js
+++ cc_5.js
@@ -8,5 +8,13 @@ let employees = [
     {name: "Josh Tho", hourlyRate: 32, hoursWorked: 35},
     {name: "Martin Hank", hourlyRate: 12, hoursWorked: 25},
 ];
+console.table(employees)
+
+// Step 3
+let calcBasePay = (rate, hours) => 
+    rate * Math.min(hours, 40);
+
+console.log(` $20 an hour for 40 hours: ${calcBasePay(20, 40)}`); 
+
 
 // Code goes here
