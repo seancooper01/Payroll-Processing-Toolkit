720c87af5fd03a41ab2367ffd7d98ff9a410eb7d
Sean Cooper <seancooper@usf.edu>
2025-09-26 20:42:49 -0400

    step 2 done

--- cc_5.js
+++ cc_5.js
@@ -2,6 +2,11 @@
 // Step 1 - Complete
 
 // Step 2
-
+let employees = [
+    {name: "Joe Shmo", hourlyRate: 20, hoursWorked: 40},
+    {name: "Karen Hilt", hourlyRate: 15, hoursWorked: 30},
+    {name: "Josh Tho", hourlyRate: 32, hoursWorked: 35},
+    {name: "Martin Hank", hourlyRate: 12, hoursWorked: 25},
+];
 
 // Code goes here
