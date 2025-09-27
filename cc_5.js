cc8111b65fd21b216fe01babf056e23ecd67c2d1
Sean Cooper <seancooper@usf.edu>
2025-09-26 21:12:51 -0400

    step 4 done

--- cc_5.js
+++ cc_5.js
@@ -8,13 +8,22 @@ let employees = [
     {name: "Josh Tho", hourlyRate: 32, hoursWorked: 35},
     {name: "Martin Hank", hourlyRate: 12, hoursWorked: 25},
 ];
-console.table(employees)
+console.table(employees) // table for employees
 
 // Step 3
 let calcBasePay = (rate, hours) => 
     rate * Math.min(hours, 40);
 
-console.log(` $20 an hour for 40 hours: ${calcBasePay(20, 40)}`); 
+console.log(` $20 an hour for 40 hours: $${calcBasePay(20, 40)}`); // test function
 
+// Step 4
+function calcOTPay(rate,hours) {
+    if (hours > 40) {
+        return (hours - 40) * (rate * 1.5); 
+    } else {
+        return 0;
+    }
+}
+console.log(`OT pay for 10 hours @ $40 an hour: $${calcOTPay(40,50)} `); // test function
 
 // Code goes here
